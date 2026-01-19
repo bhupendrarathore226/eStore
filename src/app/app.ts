e54aa6d460ab904ce958a2cb935c914df1206a10
bhupendrarathore226 <bhupendra.rathore226@gmail.com>
2026-01-19 18:29:03 -0500

    Initial commit - Angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Catnavigation } from "./components/catnavigation/catnavigation";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Catnavigation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
